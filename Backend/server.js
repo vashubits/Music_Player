fa426dedfd7bfcec9b9d131f8438c27f7e937413
vashubits <cse22_vashugupta@ggct.co.in>
2026-04-16 14:00:05 +0530

    host correction

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,11 @@
-const app = require('./src/app')
-const connectDB = require('./src/db/db')
-require('dotenv').config()
+const app = require('./src/app');
+const connectDB = require('./src/db/db');
+require('dotenv').config();
 
+const PORT = process.env.PORT || 3000;
 
-connectDB()
-app.listen(3000,()=>{
-    console.log("server start from 3000 port")
-})
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
