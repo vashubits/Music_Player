4dae843c4692ade07a9ede34db1eb303ffb15eca
vashubits <cse22_vashugupta@ggct.co.in>
2026-04-16 21:32:22 +0530

    fix: remove deprecated mongoose options

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connection successful");
   } catch (err) {
     console.error("MongoDB connection failed:", err.message);
-    process.exit(1); // optional, but at least error log clear hoga
+    process.exit(1);
   }
 }
 
